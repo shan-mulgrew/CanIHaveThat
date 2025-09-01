b5059968d5ce451a1957860462d351da16fa0af1
Shan <42028485+shan-mulgrew@users.noreply.github.com>
2025-09-01 23:06:56 +0100

    Remove duplicate useFrameworkReady import

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { useFrameworkReady } from '../hooks/useFrameworkReady'
 import { useFrameworkReady } from '@/hooks/useFrameworkReady'
 
 export default function RootLayout() {
