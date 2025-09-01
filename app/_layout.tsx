18be784204a851672a582f4ad8065891f120bfa4
Shan <42028485+shan-mulgrew@users.noreply.github.com>
2025-09-01 23:25:11 +0100

    Remove NavigationContainer wrapper

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,17 @@
 import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
 import { useFrameworkReady } from '@/hooks/useFrameworkReady'
 
 export default function RootLayout() {
   useFrameworkReady();
 
   return (
-    <NavigationContainer>
-      <>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="+not-found" />
-        </Stack>
-        <StatusBar style="auto" />
-      </>
-    </NavigationContainer>
+    <>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </>
   );
 }
