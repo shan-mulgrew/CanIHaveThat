226b1fd205d1fee6654c21988f215ea8e3541028
Shan <42028485+shan-mulgrew@users.noreply.github.com>
2025-09-01 23:05:43 +0100

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useFrameworkReady } from '../hooks/useFrameworkReady'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady'
 
 export default function RootLayout() {
   useFrameworkReady();
